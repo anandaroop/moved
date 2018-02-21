1dbe039ddeb16c0f3597d61235237ee35b4e4df3
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-02-20 23:53:36 -0500

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.h1`
+  font-size: 1em;
+`
 
 class App extends React.Component {
   render() {
-    return <h1>Hello, world</h1>
+    return <Header>Hello, world</Header>
   }
 }
 
