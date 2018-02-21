e5d90cd4202e9fe4e53ec85f36c4b7d16c77f914
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-02-21 00:13:44 -0500

    Add mapbox-gl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { MAPBOX_ACCESS_TOKEN } from './secrets'
+import mapboxgl from 'mapbox-gl'
 
-const Header = styled.h1`
-  font-size: 1em;
+mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
+
+const MapContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
 `
-
 class App extends React.Component {
+  _container
+  _map
+
+  static propTypes = {
+    initialStyle: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    initialStyle: 'mapbox/light-v9'
+  }
+
+  componentDidMount() {
+    const options = {
+      container: this._container,
+      style: `mapbox://styles/${this.props.initialStyle}`
+    }
+    this._map = new mapboxgl.Map(options)
+  }
+
+  componentWillUnmount() {
+    this._map.remove()
+  }
+
   render() {
-    return <Header>Hello, world</Header>
+    return <MapContainer innerRef={el => (this._container = el)} />
   }
 }
 
