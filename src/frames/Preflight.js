9ce6a0dea636b85aee38eeb90aadce76b28a1d11
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-02-25 22:56:43 -0500

    Add a design for Preflight

--- src/frames/Preflight.js
+++ src/frames/Preflight.js
@@ -1,29 +1,41 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-const animate = keyframes`
+const delayedFadeOut = keyframes`
   0% { opacity: 1; }
- 75% { opacity: 1; }
+ 80% { opacity: 1; }
 100% { opacity: 0; }
 `
 
-const Layout = styled.div`
+const Overlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 1;
-  background: white;
 
-  animation-name: ${animate};
+  background: white;
+  color: #666;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  animation-name: ${delayedFadeOut};
   animation-duration: ${props => `${props.duration / 1000}s`};
-  /* animation-timing-function: ; */
-  /* animation-delay: 0s; */
-  /* animation-iteration-count: ; */
-  /* animation-direction: normal; */
-  /* animation-fill-mode: forwards; */
-  /* animation-play-state: ; */
+  animation-fill-mode: forwards;
+`
+
+const Type = styled.div`
+  position: absolute;
+  font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+  font-size: 12vh;
+  line-height: 120%;
+  font-weight: 200;
+
+  animation-name: ${delayedFadeOut};
+  animation-duration: ${props => `${props.duration / 1000}s`};
+  animation-fill-mode: forwards;
 `
 
 const animateRoop = keyframes`
@@ -32,51 +44,57 @@ const animateRoop = keyframes`
 100% { opacity: 1; }
 `
 
-const Roop = styled.span`
+const Roop = styled.div`
   animation-name: ${animateRoop};
-  animation-duration: 4s;
+  animation-duration: ${props => `${props.duration / 1000}s`};
 `
 
 const animateLinta = keyframes`
    0% { opacity: 0; }
-  15% { opacity: 0; }
-  30% { opacity: 1; }
+  15% { opacity: 0; transform: translate(0px, -1vh)}
+  30% { opacity: 1; transform: translate(0px, 0px)}
  100% { opacity: 1; }
  `
 
-const Linta = styled.span`
+const Linta = styled.div`
   animation-name: ${animateLinta};
-  animation-duration: 4s;
+  animation-duration: ${props => `${props.duration / 1000}s`};
 `
 
 const animateLeela = keyframes`
    0% { opacity: 0; }
-  30% { opacity: 0; }
-  45% { opacity: 1; }
+  30% { opacity: 0; transform: translate(0px, -1vh)}
+  45% { opacity: 1; transform: translate(0px, 0px)}
  100% { opacity: 1; }
  `
 
-const Leela = styled.span`
+const Leela = styled.div`
   animation-name: ${animateLeela};
-  animation-duration: 4s;
+  animation-duration: ${props => `${props.duration / 1000}s`};
 `
 
 const animateMoved = keyframes`
    0% { opacity: 0; }
-  45% { opacity: 0; }
-  60% { opacity: 1; }
+  50% { opacity: 0; }
+  70% { opacity: 1; }
  100% { opacity: 1; }
  `
 
-const Moved = styled.span`
+const Moved = styled.div`
+  color: red;
   animation-name: ${animateMoved};
-  animation-duration: 4s;
+  animation-duration: ${props => `${props.duration / 1000}s`};
 `
 
 const Preflight = ({ duration }) => (
-  <Layout duration={duration}>
-    <Roop>A</Roop> <Linta>and B</Linta> <Leela>and C</Leela> <Moved>and D</Moved>
-  </Layout>
+  <Overlay duration={duration}>
+    <Type duration={duration}>
+      <Roop duration={duration}>Roop</Roop>
+      <Linta duration={duration}>and Linta</Linta>
+      <Leela duration={duration}>and Leela have </Leela>
+      <Moved duration={duration}>moved</Moved>
+    </Type>
+  </Overlay>
 )
 
 export { Preflight }
