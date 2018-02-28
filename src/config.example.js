557c09e0f22ba5aea951d49821313a5e4bfd3896
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-02-28 00:43:33 -0500

    Add example config

---
+++ src/config.example.js
@@ -0,0 +1,17 @@
+export const MAPBOX_ACCESS_TOKEN = 'REPLACE'
+
+export const NAMES = {
+  name1: 'REPLACE',
+  name2: 'REPLACE',
+  name3: 'REPLACE'
+}
+
+export const NEW_ADDRESS = {
+  addressee: 'REPLACE',
+  address: 'REPLACE',
+  city: 'REPLACE',
+  state: 'REPLACE',
+  postal: 'REPLACE2'
+}
+
+export const VCARDS = [{ name: 'REPLACE', filename: 'REPLACE' }, { name: 'REPLACE', filename: 'REPLACE' }]
